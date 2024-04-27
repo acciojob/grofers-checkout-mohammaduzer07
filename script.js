725c016688792aadac159bcfcd020554c5669936
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-04-27 12:31:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,27 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+	//Add your code here
+	let item = document.querySelectorAll('.item');
+	let price = document.querySelectorAll(".price");
+
+	let sum = 0;
+	for(let t of price){
+		sum += parseInt(t.innerText);
+	}
+
+	let table = document.querySelector("table");
+	let newRow = document.createElement("tr");
+	newRow.setAttribute('id', "ans");
+	let col1 = document.createElement("td");
+	col1.innerText = "Total sum is: ";
+	let col2 = document.createElement("td");
+	col2.innerText = sum;
+
+	newRow.appendChild(col1);
+	newRow.appendChild(col2);
+	table.appendChild(newRow);
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
